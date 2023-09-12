d01285ef34a9874836dcdcb9e58dc0b061244993
Ary Kurnia <aryk922@gmail.com>
2023-09-12 21:04:27 +0800

    menghapus fitur kontak kami

--- html/submit_form.php
+++ html/submit_form.php
@@ -1,23 +1,23 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $name = $_POST["name"];
+//     $email = $_POST["email"];
+//     $message = $_POST["message"];
 
-    $to = "aryk922@gmail.com";  // Ganti dengan alamat email Anda
-    $subject = "Pesan dari $name melalui Form Kontak";
-    $headers = "From: $email";
+//     $to = "aryk922@gmail.com";  // Ganti dengan alamat email Anda
+//     $subject = "Pesan dari $name melalui Form Kontak";
+//     $headers = "From: $email";
 
-    $mailContent = "Nama: $name\n";
-    $mailContent .= "Email: $email\n\n";
-    $mailContent .= "Pesan:\n$message";
+//     $mailContent = "Nama: $name\n";
+//     $mailContent .= "Email: $email\n\n";
+//     $mailContent .= "Pesan:\n$message";
 
-    if (mail($to, $subject, $mailContent, $headers)) {
-        echo "Pesan Anda telah berhasil dikirim.";
-    } else {
-        echo "Maaf, ada masalah dalam mengirim pesan.";
-    }
-} else {
-    echo "Permintaan tidak valid.";
-}
+//     if (mail($to, $subject, $mailContent, $headers)) {
+//         echo "Pesan Anda telah berhasil dikirim.";
+//     } else {
+//         echo "Maaf, ada masalah dalam mengirim pesan.";
+//     }
+// } else {
+//     echo "Permintaan tidak valid.";
+// }
 ?>
